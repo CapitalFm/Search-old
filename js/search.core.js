f9c72ec156e843693293a8713362374796979cba
Ben <ben@coterie.co.ke>
2015-04-16 14:33:13 +0300

    Create core search script

---
+++ js/search.core.js
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Capital Group Limited (http://www.capitalfm.co.ke)
+ * Copyright 1996 - 2015
+ * Author Ben <ben@coterie.co.ke>
+ */
+(function($)
+{
+	var capitalSearch = function(){};
+	
+	$( document ).ready(function()
+	{
+		new capitalSearch();
+	});
+	
+})(jQuery);
